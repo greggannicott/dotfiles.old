f4079aab88cb021b258ae71079e5415c28bac1e8
Greg Gannicott <greg@gannicott.co.uk>
2016-03-27 18:34:25 +0100

    Add .ctags file.
    The .ctags file provides support for navigation of Angular applications.

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -8,7 +8,7 @@
 
 dir=~/dotfiles                    # dotfiles directory
 olddir=~/dotfiles_old             # old dotfiles backup directory
-files="gitconfig gvimrc tmux.conf vimrc"    # list of files/folders to symlink in homedir
+files="gitconfig gvimrc tmux.conf vimrc ctags"    # list of files/folders to symlink in homedir
 
 ##########
 
